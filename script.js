7774a7072179d2ad79b95a006eb1d8e278c27d12
pratyushbehera23 <pratyushbehera.mail@gmail.com>
2024-10-18 11:18:46 +0530

    editted

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNavbar() {
+    document.getElementById("sideNavigationBar")
+        .style.width = "60%";
+}
+function closeNavbar() {
+    document.getElementById("sideNavigationBar")
+        .style.width = "0%";
+}
